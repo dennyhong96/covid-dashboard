9fb5434f3aa754e9a4e68857670353354ece161d
Haiyang Hong <hong961127@gmail.com>
2020-09-06 15:00:58 -0700

    Setup project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+import Topbar from "./ui/Topbar";
+
+import "./App.scss";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Topbar />
+    </Fragment>
+  );
+};
+
+export default App;
